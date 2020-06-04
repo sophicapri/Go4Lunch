090d48262e7c02ed342348fab2c4f00f0a1abddb
Sophie Cap <sophie.capricorne@gmail.com>
2020-06-04 17:15:21 +0200

    travis

--- app/src/test/java/com/sophieopenclass/go4lunch/ExampleUnitTest.java
+++ app/src/test/java/com/sophieopenclass/go4lunch/ExampleUnitTest.java
@@ -3,12 +3,9 @@ package com.sophieopenclass.go4lunch;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
