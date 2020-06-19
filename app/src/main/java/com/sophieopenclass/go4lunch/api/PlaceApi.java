8b4a030de76cb4445b2776c2aaa906d19abad34e
Sophie Cap <sophie.capricorne@gmail.com>
2020-06-19 22:10:18 +0200

    travis

--- app/src/main/java/com/sophieopenclass/go4lunch/api/PlaceApi.java
+++ app/src/main/java/com/sophieopenclass/go4lunch/api/PlaceApi.java
@@ -1,24 +1,25 @@
 package com.sophieopenclass.go4lunch.api;
 
-import com.sophieopenclass.go4lunch.BuildConfig;
-import com.sophieopenclass.go4lunch.models.json_to_java.RestaurantsResult;
 import com.sophieopenclass.go4lunch.models.json_to_java.PlaceDetailsResult;
+import com.sophieopenclass.go4lunch.models.json_to_java.RestaurantsResult;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface PlaceApi {
-    String apiKey = BuildConfig.API_KEY;
 
-    @GET("nearbysearch/json?&rankby=distance&type=restaurant&key=" + apiKey)
-    Call<RestaurantsResult> getNearbyPlaces(@Query("location") String location);
+    @GET("nearbysearch/json?&rankby=distance&type=restaurant")
+    Call<RestaurantsResult> getNearbyPlaces(@Query("location") String location,
+                                            @Query("key") String apiKey);
 
-    @GET("nearbysearch/json?&key=" + apiKey)
-    Call<RestaurantsResult> getMoreNearbyPlaces(@Query("pagetoken") String nextPageToken);
+    @GET("nearbysearch/json?")
+    Call<RestaurantsResult> getMoreNearbyPlaces(@Query("pagetoken") String nextPageToken,
+                                                 @Query("key") String apiKey);
 
-    @GET("details/json?&key=" + apiKey)
+    @GET("details/json?")
     Call<PlaceDetailsResult> getPlaceDetails(@Query("place_id") String placeId,
-                                            @Query("language") String language);
+                                            @Query("language") String language,
+                                             @Query("key") String apiKey);
 }
 
